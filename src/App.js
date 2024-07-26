e426217c6ed711acaf9be9fa1b29e8e95d3b9318
manuo <ojhapranav1999@gmail.com>
2024-07-26 22:57:06 +0530

    Commiting - first commit for all the files in the src folder .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {  BrowserRouter as Router,Routes,Route,redirect} from "react-router-dom";
+import { Home } from './user/Home';
+import { NewPlaces } from './places/pages/NewPlaces';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Router>
+    <Routes>
+
+<Route path='/' Component={Home}>
+</Route>
+<Route path='/places/new' Component={NewPlaces}></Route>
+    </Routes>
+  </Router>
 }
 
 export default App;
