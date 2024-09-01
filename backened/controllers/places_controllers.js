44c3e0ea2c5e0fc6e6e81faf16b57b9f5e31259d
manuo <ojhapranav1999@gmail.com>
2024-09-01 22:07:16 +0530

    adding the express validator libraries and validator functionalities.

--- backened/controllers/places_controllers.js
+++ backened/controllers/places_controllers.js
@@ -1,6 +1,7 @@
 
 const HttpError = require('../models/http_error')
 const {v4:uuid} = require('uuid');
+const {validationResult} = require('express-validator');
 
 let DUMMY_PLACES = [
     {
@@ -43,6 +44,11 @@ const getPlacesByUserId = (req,res,next)=>{
 
 
 const createPlace = (req,res,next)=>{
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        throw new HttpError('Invalid inputs passed , please check your data',422);
+    }
     console.log('post request received')
     // console.log(req.body)
     const {title,description,coordinates,address,creator} = req.body;
@@ -75,6 +81,9 @@ const updatePlace = (req,res,next)=>{
 
 const deletePlace = (req,res,next)=>{
     const placeId = req.params.pid;
+    if(!DUMMY_PLACES.find(p=>p=== placeId)){
+        throw new HttpError('Could not find the place for that id.' , 404);
+    }
     DUMMY_PLACES = DUMMY_PLACES.filter(p=>p.id!==placeId);
     res.status(200).json({message:'Deleted Place.'});
 };
--- backened/controllers/users_controller.js
+++ backened/controllers/users_controller.js
@@ -1,5 +1,6 @@
 
 const HttpError = require('../models/http_error')
+const {validationResult} = require('express-validator');
 // const uuid = require('uuid/v')
 const DUMMY_USERS = [
     {
@@ -17,6 +18,11 @@ const getUsers = (req,res,next)=>{
 
 
 const signup = (req,res,next)=>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        throw new HttpError('Invalid inputs passed , please check your data',422);
+    }
     const {name,email,password} = req.body;
 
     const hasUser = DUMMY_USERS.find(u=>u.emails===email)
