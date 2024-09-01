b7d9378d347bb0394aa5d7f067efc3ab1f497a5f
manuo <ojhapranav1999@gmail.com>
2024-09-01 22:08:12 +0530

    adding the express validator libraby code for input validation.

--- backened/routes/users_routes.js
+++ backened/routes/users_routes.js
@@ -10,12 +10,16 @@ const dummy_user = [{
 
 
 
-const usersControllers = require('../controllers/users_controller')
+const usersControllers = require('../controllers/users_controller');
+const { check } = require('express-validator');
 
 router.get('/',usersControllers.getUsers)
 
 
-router.post('/signup',usersControllers.signup);
+router.post('/signup',[
+    check('name').not().isEmpty(),check('email').normalizeEmail().isEmail(),
+    check('password').isLength({min:6})
+],usersControllers.signup);
 
 router.post('/login',usersControllers.login);
 
