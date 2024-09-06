c1a0e651d6da1213c81e347d727c08838b2eeb65
manuo <ojhapranav1999@gmail.com>
2024-09-06 22:21:07 +0530

    adding the code for the coordinates of the addresses

---
+++ backened/Utils/location.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+const API_KEY = 'faksdnfasdkn12n3';
+
+async function getCoordsForAddress (address){
+    return {lat:40.7484405,
+        long:-73.9882393};
+
+    //code for using google map api
+
+}
+
+
